38693b81d1638ee42566a40f6af1d5b0015d8ee9
longgu02 <phamlong121120022@gmail.com>
2025-10-19 22:28:06 +0700

    Initial commit

---
+++ apps/c4i-fe/app/app.tsx
@@ -0,0 +1,13 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.scss';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@c4i-fe/c4i-fe" />
+    </div>
+  );
+}
+
+export default App;
